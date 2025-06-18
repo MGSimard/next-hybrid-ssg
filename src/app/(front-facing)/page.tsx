d39211cef89e32b4fe5079762ef7c80885689ecd
MGSimard <mgsimard.dev@gmail.com>
2025-06-18 19:47:04 -0400

    a

--- src/app/(front-facing)/page.tsx
+++ src/app/(front-facing)/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function PageHome() {
   return (
     <main>
       <h1>Hello World</h1>
+      <Link href="/dashboard">Dashboard Link</Link>
     </main>
   );
 }
