0e47072694370e4fbd7cb7a32e934bbfbfbc0a1c
MGSimard <mgsimard.dev@gmail.com>
2025-06-19 17:47:10 -0400

    Swap to this sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,16 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { SiteHeader } from "@/components/site-header";
+import { AppSidebar } from "@/components/sidebar/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { BreadcrumbHeader } from "@/components/sidebar/breadcrumb-header";
 
 export default function LayoutDashboard({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider className="flex flex-col">
-      <SiteHeader />
       <div className="flex flex-1">
         <AppSidebar />
-        <SidebarInset>{children}</SidebarInset>
+        <SidebarInset>
+          <BreadcrumbHeader />
+          <div className="max-w-8xl w-full mx-auto flex flex-1 flex-col gap-4 p-4">{children}</div>
+        </SidebarInset>
       </div>
     </SidebarProvider>
   );
