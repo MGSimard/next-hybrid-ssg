6f02fd991be4dfe470913887dd8ecc952919aa9c
MGSimard <mgsimard.dev@gmail.com>
2025-06-20 09:13:34 -0400

    border-b

--- src/components/sidebar/breadcrumb-header.tsx
+++ src/components/sidebar/breadcrumb-header.tsx
@@ -12,7 +12,7 @@ import { ThemeToggle } from "@/components/sidebar/theme-toggle";
 
 export function BreadcrumbHeader() {
   return (
-    <header className="flex h-16 px-6 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+    <header className="flex h-16 px-6 shrink-0 items-center justify-between gap-2 border-b transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2">
         <SidebarTrigger className="-ml-1 cursor-pointer" />
         <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
