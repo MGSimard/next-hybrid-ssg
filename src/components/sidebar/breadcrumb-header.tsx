5fafebf2d777023621454c19c55325cbbde13d13
MGSimard <mgsimard.dev@gmail.com>
2025-06-20 09:04:46 -0400

    Specificity

--- src/components/sidebar/breadcrumb-header.tsx
+++ src/components/sidebar/breadcrumb-header.tsx
@@ -15,7 +15,7 @@ export function BreadcrumbHeader() {
     <header className="flex h-16 px-4 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2">
         <SidebarTrigger className="-ml-1 cursor-pointer" />
-        <Separator orientation="vertical" className="mr-2 h-6" />
+        <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
         <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem className="hidden md:block">
